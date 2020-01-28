22a875886b20ea559cd7192d8bbb15d9e5af8965
Christina Chapman <cmchapman75@gmail.com>
2020-01-28 13:32:23 -0600

    BST

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const BinarySearchTree = require('./BinarySearchTree');
 
 // 3. Create a BST class
 // Walk through the binary search tree code in 
@@ -13,3 +13,24 @@
 // Create a binary search tree called BST and insert
 // E A S Y Q U E S T I O N into your tree. Compare 
 // your result with the result from the 1st exercise.
+
+
+const BST = function(key, value) {
+  if (this.key == null) {
+    this.key = key;
+    this.value = value;
+  } else if (key < this.key) {
+    if (this.left == null){
+      this.left = new BinarySearchTree(key, value, this);
+    } else {
+      this.left.insert(key, value);
+    }
+  } else {
+    if (this.right == null) {
+      this.right = new BinarySearchTree(key, value, this);
+    } else {
+      this.right.insert(key, value);
+    }
+  }
+};
+ BST()
