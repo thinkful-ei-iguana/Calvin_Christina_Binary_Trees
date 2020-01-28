54653b4ed1fa71ac000e1b350a11c332cd19a868
Christina Chapman <cmchapman75@gmail.com>
2020-01-28 11:38:30 -0600

    added steps for question 3

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-// 1. Draw a BST
-// Given the data 3,1,4,6,9,2,5,7, if you were to insert this into an empty binary search tree, what would the tree look like? (Draw the tree, no coding needed here.)
-// Draw the BST with the keys - E A S Y Q U E S T I O N
+
+
+// 3. Create a BST class
+// Walk through the binary search tree code in 
+// the curriculum and understand it well. Then 
+// write a BinarySearchTree class with its core 
+// functions (insert(), remove(), find()) from 
+// scratch.
+
+// Create a binary search tree called BST and insert 
+// 3,1,4,6,9,2,5,7 into your tree. Compare your
+//  result with the result from the 1st exercise.
+// Create a binary search tree called BST and insert
+// E A S Y Q U E S T I O N into your tree. Compare 
+// your result with the result from the 1st exercise.
