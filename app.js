a8f3ac393da85f1065aceb9408c2abeea04262a6
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-28 12:44:09 -0700

    clearing up code

--- app.js
+++ app.js
@@ -52,7 +52,12 @@ function letterTree() {
 // console.log();
 
 //4 - What does this program do?
-
+// function tree(t){
+//     if(!t){
+//         return 0;
+//     }
+//     return tree(t.left) + t.value + tree(t.right)
+// }
 //  This function sums the values of all of the nodes in the tree. //
 
 //5 - Height of a BST
@@ -83,37 +88,3 @@ console.log(height(numberTree()));
 //8 - Balanced BST Algorithm
 
 //9 - Are they the same BSTs?
-
-// // 3. Create a BST class
-// // Walk through the binary search tree code in
-// // the curriculum and understand it well. Then
-// // write a BinarySearchTree class with its core
-// // functions (insert(), remove(), find()) from
-// // scratch.
-
-// // Create a binary search tree called BST and insert
-// // 3,1,4,6,9,2,5,7 into your tree. Compare your
-// //  result with the result from the 1st exercise.
-// // Create a binary search tree called BST and insert
-// // E A S Y Q U E S T I O N into your tree. Compare
-// // your result with the result from the 1st exercise.
-
-// const BST = function(key, value) {
-//   if (this.key == null) {
-//     this.key = key;
-//     this.value = value;
-//   } else if (key < this.key) {
-//     if (this.left == null) {
-//       this.left = new BinarySearchTree(key, value, this);
-//     } else {
-//       this.left.insert(key, value);
-//     }
-//   } else {
-//     if (this.right == null) {
-//       this.right = new BinarySearchTree(key, value, this);
-//     } else {
-//       this.right.insert(key, value);
-//     }
-//   }
-// };
-// BST();
